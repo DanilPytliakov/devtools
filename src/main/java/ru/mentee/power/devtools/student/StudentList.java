f0a1d61753880cff745855fa8f8014ebf55a0878
Данил Пытляков <danilpytlikov@gamil.com>
2026-02-03 22:44:21 +0300

    ``` fix: исправить проблемы найденные в само-ревью DVT-9
    Добавлен CI badge в README
    Удалён отладочный вывод из StudentList.java
    Удалён закомментированный код из этого же файла ```

--- src/main/java/ru/mentee/power/devtools/student/StudentList.java
+++ src/main/java/ru/mentee/power/devtools/student/StudentList.java
@@ -13,18 +13,10 @@ public class StudentList {
     public void addStudent(Student student) {
         if (student != null) {  // нарушение: нет пробела после if
             studentList.add(student);
-            System.out.println("Debug: student was added");
         }
     }
 
     public List<Student> getStudentsByCity(String city) {
-        /*
-        for (Student s : studentList) {
-            if(s.city().equals(city)){
-                studentList.add(s);
-        }
-        return.studentList;
-        */
         return studentList.stream().
         filter(s -> s.city().equals(city)).
         toList();
